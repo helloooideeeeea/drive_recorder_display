12428655c9f012a244c3cc2e3d8169bfa968c819
Yasushi Sakita <yasushi.sakita@gmail.com>
2022-05-21 10:22:20 +0900

    修正

--- Library/Wifi.py
+++ Library/Wifi.py
@@ -4,23 +4,21 @@ from loguru import logger
 
 class Wifi:
 
-    def __init__(self, ssid, password):
-        self.ssid = ssid
-        self.password = password
+    def __init__(self):
         self.interface_name = "wlan0"
-        self.main_dict = {}
 
     def search(self):
-        command = """sudo iwlist {} scan | grep -ioE 'ssid:"(.*{}.*)'"""
-        result = os.popen(command.format(self.interface_name, self.ssid))
+        # sudo iwlist wlan0 scan | grep -ioE 'ssid:(.*)'
+        command = """sudo iwlist {} scan | grep -ioE 'ssid:(.*)'"""
+        result = os.popen(command.format(self.interface_name))
         result = list(result)
         if "Device or resource busy" in result:
             raise Exception()
         ssid_list = [item.lstrip('SSID:').strip('"\n') for item in result]
         return ssid_list
 
-    def connection(self, ssid):
-        cmd = "iwconfig {} password {} iface {}".format(ssid, self.password, self.interface_name)
+    def connection(self, ssid, password):
+        cmd = "iwconfig {} password {} iface {}".format(ssid, password, self.interface_name)
         if os.system(cmd) != 0:
             logger.info("Couldn't connect to ssid:{}".format(ssid))
             raise Exception()
